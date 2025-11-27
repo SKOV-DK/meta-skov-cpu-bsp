SKOV-DK/meta-skov-cpu-bsp

commit a6c05eaea8975244be4f7ab8e2a74061d6d2c7d4

factory-install: no resize of a non-existant filesystem on the home partition
As the filesystem is only going to be created on first boot there is nothing to
be done anymore.

recipes-support/factory-install/factory-install/factory-install.sh
@@ -88,11 +88,6 @@ if [ -n "${PART_NO}" ] ; then
    if ! parted --script "${TGT_DEV}" -- resizepart "${PART_NO}" -64s; then
       failure "Failed to expand ${PART_NO}th partition of ${MEDIUM} to the end of the device."
    fi
-
-   echo "Resizing ext4 on ${PART_NO}th partition of ${MEDIUM} accordingly."
-   if ! resize2fs -p "${TGT_DEV}"p"${PART_NO}"; then
-      failure "Failed to resize ext4 on ${PART_NO}th partition of ${MEDIUM} accordingly."
-   fi
 fi
 
 echo "Disabling the ${MEDIUM}'s first boot partition's read-only mode."

commit 01fd66b52c6c70a2b4b845289246a73aca85d630

factory-install: resize ext4 on home partition for non-secure-boot machine
At least for the imx8-cpu /home still needs to be grown to fill the whole
partition.

Fixes: a6c05eaea897 ("factory-install: no resize of a non-existant filesystem on the home partition")

recipes-support/factory-install/factory-install.bb
@@ -19,6 +19,8 @@ TGT_MMC = "mmcblk2"
 PART_NO ?= ""
 PART_NO:imx8-cpu = "6"
 PART_NO:imx8s-cpu = "9"
+RESIZE = "true"
+RESIZE:imx8s-cpu = "false"
 
 WIC_IMAGE:imx8-cpu = "${FACTORY_INSTALL_IMAGE}"
 WIC_IMAGE:imx8s-cpu = "${FACTORY_INSTALL_IMAGE}-secbootimg"
@@ -64,6 +66,7 @@ do_install() {
         --expression=s,@BAREBOX_RENAME@,${BAREBOX_RENAME}, \
         --expression=s,@DATADIR@,${datadir}/factory-install/, \
         --expression=s,@PART_NO@,${PART_NO}, \
+        --expression=s,@RESIZE@,${RESIZE}, \
         --in-place ${D}${bindir}/factory-install.sh
 
     install -d ${D}${datadir}/factory-install

recipes-support/factory-install/factory-install/factory-install.sh
@@ -25,6 +25,7 @@ WIC=@WIC@
 BAREBOX_RENAME=@BAREBOX_RENAME@
 DATADIR=@DATADIR@
 PART_NO=@PART_NO@
+RESIZE=@RESIZE@
 
 
 show_splash () {
@@ -87,6 +88,13 @@ if [ -n "${PART_NO}" ] ; then
    echo "Expanding ${PART_NO}th partition of ${MEDIUM} to the end of the device."
    if ! parted --script "${TGT_DEV}" -- resizepart "${PART_NO}" -64s; then
       failure "Failed to expand ${PART_NO}th partition of ${MEDIUM} to the end of the device."
+   fi
+
+   if [ "${RESIZE}" = "true" ] ; then
+      echo "Resizing ext4 on ${PART_NO}th partition of ${MEDIUM} accordingly."
+      if ! resize2fs -p "${TGT_DEV}"p"${PART_NO}"; then
+         failure "Failed to resize ext4 on ${PART_NO}th partition of ${MEDIUM} accordingly."
+      fi
    fi
 fi
 